943404acf697e55876f9939fed50b03010b21c13
Milind <gunjimilind@gmail.com>
2018-08-07 22:03:40 +0530

    ApiKey API

---
+++ src/api/user/sgc_apikey.php
@@ -0,0 +1,88 @@
+<?php
+
+	namespace SGCSdk\api\user;
+
+	defined('_SMSGATEWAYCENTRE_ACCESS') OR exit('Direct access to this location is not allowed.');
+
+	/*	 * *******smsgatewaycentre******* * */
+
+	use SGCSdk\object\sgc_auth;
+	use SGCSdk\config\sgc_common_api_params;
+	use SGCSdk\config\sgc_user_apikey_api_params;
+	use SGCSdk\lib\sgc_callapi;
+	use SGCSdk\config\sgc_constant;
+	use SGCSdk\object\sgc_user;
+
+	class sgc_apikey {
+
+		private $auth;
+		private $user;
+		private $header;
+		private $data;
+		private $useRestApi = false;
+
+		/**
+		 * Enable/Disable RestAPI
+		 * @param type $useRestApi
+		 */
+		function useRestApi($useRestApi) {
+			$this->useRestApi = $useRestApi;
+		}
+
+		/**
+		 * Main Constructor
+		 * @param Auth $auth
+		 * @param User $user
+		 */
+		function __construct(sgc_auth $auth, sgc_user $user) {
+			$this->auth = $auth;
+			$this->user = $user;
+			$this->header = [
+				sgc_common_api_params::API_COMMON_PARAM_API_KEY => $this->auth->getApiKey()
+			];
+
+			$this->data = [
+				sgc_common_api_params::API_COMMON_PARAM_USERID => $this->auth->getUsername(),
+				sgc_common_api_params::API_COMMON_PARAM_PASSWORD => $this->auth->getPassword(),
+				sgc_common_api_params::API_COMMON_PARAM_OUTPUT_FORMAT => $this->user->getOutput()
+			];
+		}
+
+		/**
+		 * Create SenderId
+		 * @return type
+		 */
+		function Create() {
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_APIKEY_CREATE, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_POST, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+		/**
+		 * Read SenderId
+		 * @return type
+		 */
+		function Read() {
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_APIKEY_READ, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_GET, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+		/**
+		 * Update SenderId
+		 * @return type
+		 */
+		function Update() {
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_APIKEY_UPDATE, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_POST, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+		/**
+		 * Delete SenderId
+		 * @return type
+		 */
+		function Delete() {
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_APIKEY_DELETE, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_POST, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+	}
+	
---
+++ test/user/apikey.php
@@ -0,0 +1,42 @@
+<?php
+
+	define("_SMSGATEWAYCENTRE_ACCESS", true);
+
+	ini_set("display_errors", 1);
+
+	//AUTOLOAD CLASS
+	require_once '../../vendor/autoload.php';
+
+	//SET AUTH
+	$Auth = new \SGCSdk\object\sgc_auth();
+	$Auth->setUsername("admin");
+	$Auth->setPassword("admin123");
+
+	//SET PARAMS
+	$User = new \SGCSdk\object\sgc_user();
+	$User->setOutput("json");
+
+	//INITIATE HANDLE
+	$request = new \SGCSdk\api\user\sgc_apikey($Auth, $User);
+
+	//If you want to use true rest api like DELETE, PATCH, POST , GET
+	$request->useRestApi(true);
+
+	//Create apiKey
+	echo "Create apiKey: " . $request->Create();
+	echo "<br />";
+	echo "<br />";
+
+	//Read apiKey list
+	echo "Read apiKey: " . $request->Read();
+	echo "<br />";
+	echo "<br />";
+
+	//Update apiKey
+	echo "Update apiKey: " . $request->Update();
+	echo "<br />";
+	echo "<br />";
+
+	//Delete apiKey
+	echo "Delete apiKey: " . $request->Delete();
+	
