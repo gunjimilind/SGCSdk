d5a740c377e8a2c9f2253dd2a9a45bb196d8e548
Milind <gunjimilind@gmail.com>
2018-08-11 19:19:47 +0530

    Credit Management API

---
+++ src/api/reseller/sgc_credit_management.php
@@ -0,0 +1,88 @@
+<?php
+
+	namespace SGCSdk\api\reseller;
+
+defined('_SMSGATEWAYCENTRE_ACCESS') OR exit('Direct access to this location is not allowed.');
+
+	/*	 * *******smsgatewaycentre******* * */
+
+	use SGCSdk\object\sgc_auth;
+	use SGCSdk\config\sgc_common_api_params;
+	use SGCSdk\config\sgc_reseller_credit_management_api_params;
+	use SGCSdk\lib\sgc_callapi;
+	use SGCSdk\config\sgc_constant;
+	use SGCSdk\object\sgc_reseller;
+
+	class sgc_credit_management {
+
+		private $auth;
+		private $reseller;
+		private $header;
+		private $data;
+		private $useRestApi = false;
+
+		/**
+		 * Enable/Disable RestAPI
+		 * @param type $useRestApi
+		 */
+		function useRestApi($useRestApi) {
+			$this->useRestApi = $useRestApi;
+		}
+
+		/**
+		 * Main Constructor
+		 * @param Auth $auth
+		 * @param User $user
+		 */
+		function __construct(sgc_auth $auth, sgc_reseller $reseller) {
+			$this->auth = $auth;
+			$this->reseller = $reseller;
+			$this->header = [
+				sgc_common_api_params::API_COMMON_PARAM_API_KEY => $this->auth->getApiKey()
+			];
+
+			$this->data = [
+				sgc_common_api_params::API_COMMON_PARAM_USERID => $this->auth->getUsername(),
+				sgc_common_api_params::API_COMMON_PARAM_PASSWORD => $this->auth->getPassword(),
+				sgc_common_api_params::API_COMMON_PARAM_OUTPUT_FORMAT => $this->reseller->getOutput()
+			];
+		}
+
+		/**
+		 * Read User Credit Usage
+		 * @return type
+		 */
+		function ReadUserCreditUsage() {
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_FROM_DATE] = $this->reseller->getFromDate();
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_TO_DATE] = $this->reseller->getToDate();
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_USER_LOGIN_NAME] = $this->reseller->getUserLoginName();
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_RESELLER_READ_CREDIT_HISTORY, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_POST, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+		/**
+		 * Add Credit
+		 * @return type
+		 */
+		function AddCredit() {
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_CREDIT] = $this->reseller->getCredit();
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_COMMENT] = $this->reseller->getComment();
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_USER_LOGIN_NAME] = $this->reseller->getUserLoginName();
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_RESELLER_ADD_CREDIT, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_POST, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+		/**
+		 * Add Credit
+		 * @return type
+		 */
+		function RemoveCredit() {
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_CREDIT] = $this->reseller->getCredit();
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_COMMENT] = $this->reseller->getComment();
+			$this->data[sgc_reseller_credit_management_api_params::API_RESELLER_CREDIT_MANAGEMENT_USER_LOGIN_NAME] = $this->reseller->getUserLoginName();
+			$response = new sgc_callapi(sgc_constant::SGC_API, sgc_constant::SGC_ENDPOINT_RESELLER_REMOVE_CREDIT, $this->data, $this->header, sgc_common_api_params::API_COMMON_METHOD_POST, $this->useRestApi);
+			return $response->getResponse();
+		}
+
+	}
+	
--- test/reseller/credit_management.php
+++ test/reseller/credit_management.php
@@ -1,8 +1,48 @@
 <?php
 
-	/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+	define("_SMSGATEWAYCENTRE_ACCESS", true);
 
+	ini_set("display_errors", 1);
+
+	//AUTOLOAD CLASS
+	require_once '../../vendor/autoload.php';
+
+	//SET AUTH
+	$Auth = new \SGCSdk\object\sgc_auth();
+	$Auth->setUsername("admin");
+	$Auth->setPassword("Admin@123");
+
+	//SET PARAMS
+	$Reseller = new \SGCSdk\object\sgc_reseller();
+	$Reseller->setOutput("json");
+
+	//INITIATE HANDLE
+	$request = new \SGCSdk\api\reseller\sgc_credit_management($Auth, $Reseller);
+
+	//If you want to use true rest api like DELETE, PATCH, POST , GET
+	$request->useRestApi(true);
+
+	//Read Credit History
+	$Reseller->setFromDate("2018-08-01");
+	$Reseller->setToDate("2018-08-30");
+	$Reseller->setUserLoginName("admin");
+	echo "Read Credit History:" . $request->ReadUserCreditUsage();
+	echo "<br />";
+	echo "<br />";
+
+	//Add Credit
+	$Reseller->setCredit(1234);
+	$Reseller->setComment("Test from API");
+	$Reseller->setUserLoginName("admin");
+	echo "Add Credit:" . $request->AddCredit();
+	echo "<br />";
+	echo "<br />";
+
+	//Remove Credit
+	$Reseller->setCredit(1234);
+	$Reseller->setComment("Test from API");
+	$Reseller->setUserLoginName("admin");
+	echo "Add Credit:" . $request->RemoveCredit();
+	echo "<br />";
+	echo "<br />";
+	
